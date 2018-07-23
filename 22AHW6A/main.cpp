05a5bec9c0ccdb5e6e9bcd3d05b8e4372a12b2ae
Albert Liu <albertliu@demos-macbook-pro.local>
2018-07-23 11:15:09 -0700

    Initial Commit

---
+++ 22AHW6A/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  22AHW6A
+//
+//  Created by Albert Liu on 7/23/18.
+//  Copyright © 2018 Albert Liu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
